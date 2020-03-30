f4afef4327db1f78499cb665a9a12b3c592cde80
Sam Hornett <sam.hornett@gmail.com>
2020-03-30 22:48:40 +0100

    Added Persistance

--- plus1bot.py
+++ plus1bot.py
@@ -1,4 +1,4 @@
-from telegram.ext import MessageHandler, Filters, StringRegexHandler
+from telegram.ext import MessageHandler, Filters, StringRegexHandler, PicklePersistence
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
 import logging
@@ -23,7 +23,9 @@ def echo(update, context):
 def get_album_info_from_url(shared_url):
     track_id = shared_url.split("?")[0].split("/")[-1]
     print(track_id)
-    spotify_token = "BQCrVdTlytjqg8DkGKmlJHKhuFknaF2EsSWbX6kKKR7Tb34GEYlP3GP5O1hY23H1aMdmOTUoWNxBoIGCicsxYqf49faAKm7qRg9KF40lHrXhA2NM1ftoedBHyqEKxEfFoc1OBGMk7w"
+    with open("secrets.json", 'r') as f:
+        spotify_token = json.load(f)["spotify_token"]
+
 
     headers = {
         'Accept': 'application/json',
@@ -44,6 +46,7 @@ def get_album_info_from_url(shared_url):
 
     return spotify_data['name'], spotify_data['artists'][0]['name']
 
+
 def count_plus1(update, context):
     context.user_data .setdefault("-1_given", 0)
     context.user_data.setdefault("+1_given", 0)
@@ -53,11 +56,11 @@ def count_plus1(update, context):
     context.chat_data.setdefault("Songs_recommended", {})
     context.chat_data.setdefault("Songs_recommended_total", {})
 
-    number_finder=re.compile('[+-]\d*')
-    m=number_finder.match(update.message.text)
+    number_finder = re.compile('[+-]\d*')
+    m = number_finder.match(update.message.text)
     if m:
         try:
-            number=int(m.group())
+            number = int(m.group())
         except ValueError:
             pass
         else:
@@ -71,27 +74,29 @@ def count_plus1(update, context):
             if update.message.reply_to_message:
                 if len(update.message.reply_to_message.entities) > 0:
                     if update.message.reply_to_message.entities[0]["type"] == "url":
-                        url=update.message.reply_to_message.text
+                        url = update.message.reply_to_message.text
                         if url.startswith("https://open.spotify.com"):
-                            song_name, artist=get_album_info_from_url(url)
+                            song_name, artist = get_album_info_from_url(url)
 
-                            text="{0!s} has given {1:+d} to {2!s} by {3!s}".format(
+                            text = "{0!s} has given {1:+d} to {2!s} by {3!s}".format(
                                 update.effective_user.first_name, number, song_name, artist)
                             context.chat_data["Songs_recommended"].setdefault(
-                                "{} by {}".format(song_name,artist), 0)
-                            context.chat_data["Songs_recommended"]["{} by {}".format(song_name,artist)] += 1
+                                "{} by {}".format(song_name, artist), 0)
+                            context.chat_data["Songs_recommended"]["{} by {}".format(
+                                song_name, artist)] += 1
                             context.chat_data["Songs_recommended_total"].setdefault(
-                                "{} by {}".format(song_name,artist), 0)
-                            context.chat_data["Songs_recommended_total"]["{} by {}".format(song_name,artist)] += number
-
+                                "{} by {}".format(song_name, artist), 0)
+                            context.chat_data["Songs_recommended_total"]["{} by {}".format(
+                                song_name, artist)] += number
 
                 else:
-                    text="{0!s} has given {1:+d} to {2!s}".format(
+                    text = "{0!s} has given {1:+d} to {2!s}".format(
                         update.effective_user.first_name, number, update.message.reply_to_message.from_user.first_name)
             else:
                 text = "{0!s} has given {1:+d} ".format(
                     update.effective_user.first_name, number)
-            context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text=text)
 
 
 def helpfunc(update, context):
@@ -105,12 +110,14 @@ def helpfunc(update, context):
 def songs(update, context):
     text = "Most upvoted urls \n"
     try:
-        sorted_dict = {k: v for k, v in sorted(context.chat_data["Songs_recommended"].items(), key=lambda item: item[1])}
+        sorted_dict = {k: v for k, v in sorted(
+            context.chat_data["Songs_recommended"].items(), key=lambda item: item[1])}
     except KeyError:
         text = "No scores yet!!"
     else:
-        for song,score in sorted_dict.items():
-            text += "{} got {} upvotes for a total of {} \n".format(song,score,context.chat_data["Songs_recommended_total"][song] )
+        for song, score in sorted_dict.items():
+            text += "{} got {} upvotes for a total of {} \n".format(
+                song, score, context.chat_data["Songs_recommended_total"][song])
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
@@ -132,10 +139,12 @@ if __name__ == "__main__":
         token = json.load(f)["telegram_token"]
 
     number_finder = re.compile('[+-]\d*')
+    persistence = PicklePersistence(filename="botdata")
     updater = Updater(
-        token=token, use_context=True)
+        token=token, use_context=True, persistence=persistence)
     dispatcher = updater.dispatcher
-    count_handler = MessageHandler(Filters.text, count_plus1)
+    count_handler = MessageHandler(Filters.all, count_plus1)
+
     for command in commands:
         dispatcher.add_handler(CommandHandler(
             command['command'], command['function']))
